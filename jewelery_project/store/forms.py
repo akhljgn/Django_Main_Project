2c9ae7f91dd9cf3ea29ba45e0c3d995ca0f93307
akhljgn <rakhiljagan.bca.2020@gmail.com>
2024-03-20 13:07:30 +0530

    changes

--- jewelery_project/store/forms.py
+++ jewelery_project/store/forms.py
@@ -2,7 +2,7 @@ from django.contrib.auth import password_validation
 from store.models import Address
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UsernameField, PasswordChangeForm, PasswordResetForm, SetPasswordForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UsernameField, PasswordChangeForm
 # from django.db import models
 # from django.db.models import fields
 # from django.forms import widgets
